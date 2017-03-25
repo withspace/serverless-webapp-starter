d150a731fea6182ad69a6859079825276caced17
dzikowski <jakub.dzikowski@softwaremill.com>
2017-03-25 13:10:23 +0100

    block /profile route for a non-logged-in user

--- app/Routes.js
+++ app/Routes.js
@@ -1,12 +1,21 @@
 import React from "react";
-import {Route} from "react-router-dom";
+import {Route, Redirect} from "react-router-dom";
 import {Home} from "./home/Home";
 import {Profile} from "./profile/Profile";
 
-const Routes = () => (
+const PrivateRoute = ({component: Component, user, ...rest}) => (
+  <Route
+    {...rest}
+    render={(props) => user.signedIn === true
+      ? <Component {...props} />
+      : <Redirect to={{pathname: '/', state: {from: props.location}}}/>}
+  />
+);
+
+const Routes = (props) => (
   <div>
     <Route exact path="/" component={Home}/>
-    <Route path="/profile" component={Profile}/>
+    <PrivateRoute path="/profile" component={Profile} user={props.user}/>
   </div>
 );
 
