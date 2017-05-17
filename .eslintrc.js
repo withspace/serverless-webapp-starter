b1963acc4bfe152b8a351058974bec074733eb05
dzikowski <jakub.dzikowski@softwaremill.com>
2017-05-17 19:28:59 +0200

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "parser": "babel-eslint",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
+  template: `${__dirname}/app/index.html`,
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 const webpack = require('webpack');
 
-const noEnvVar = name => {
-  throw `${name} environment variable is undefined. Please provide it to fix this error.`;
+const noEnvVar = (name) => {
+  throw new Error(`${name} environment variable is undefined. Please provide it to fix this error.`);
 };
 
 const DefinePluginConfig = new webpack.DefinePlugin({
   COGNITO_POOL_ID: JSON.stringify(process.env.COGNITO_POOL_ID || noEnvVar('COGNITO_POOL_ID')),
-  COGNITO_APP_CLIENT_ID: JSON.stringify(process.env.COGNITO_APP_CLIENT_ID || noEnvVar('COGNITO_APP_CLIENT_ID'))
+  COGNITO_APP_CLIENT_ID: JSON.stringify(process.env.COGNITO_APP_CLIENT_ID || noEnvVar('COGNITO_APP_CLIENT_ID')),
 });
 
 const BabelLoader = {
   test: /\.js$/,
   exclude: /node_modules/,
-  loader: "babel-loader"
+  loader: 'babel-loader',
 };
 
 const CSSLoader = {
   test: /\.css$/,
-  loader: "style-loader!css-loader?modules!postcss-loader"
+  loader: 'style-loader!css-loader?modules!postcss-loader',
 };
 
 const JSONLoader = {
   test: /\.json$/,
-  loader: 'json-loader'
+  loader: 'json-loader',
 };
 
 module.exports = {
   entry: [
-    './app/index.js'
+    './app/index.js',
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: "index_bundle.js"
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
   },
   module: {
-    loaders: [BabelLoader, CSSLoader, JSONLoader]
+    loaders: [BabelLoader, CSSLoader, JSONLoader],
   },
-  plugins: [HtmlWebpackPluginConfig, DefinePluginConfig]
+  plugins: [HtmlWebpackPluginConfig, DefinePluginConfig],
 };
