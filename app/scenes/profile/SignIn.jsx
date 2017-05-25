50ed9ccfcf54e6891a6fee5afbfd7dbdfe54f3c5
dzikowski <jakub.dzikowski@softwaremill.com>
2017-05-25 18:02:11 +0200

    update PropTypes to new React version, cleanup

--- app/scenes/profile/SignIn.jsx
+++ app/scenes/profile/SignIn.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'react-toolbox/lib/button';
 import { Input } from 'react-toolbox/lib/input';
 import { ErrorMessage, Loader } from '../../components/messages';
